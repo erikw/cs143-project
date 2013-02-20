a048ab2768e9067539d943bfce0b991d24388aba
the911s <checkeredflask@hotmail.com>
2013-02-20 10:20:15 -0800

    Added some work on RR. Still has bugs
    Bugs occur when jobs are removed. Need to adjust the currentJob ptr when
    jobs are removed, and possibly do something to the time slice to get it
    to reset (if a job finishes at t=3 in a timeslice, the next job gets 10,
    not 7 time units)

--- src/com/jimweller/cpuscheduler/RoundRobinSchedulingAlgorithm.java
+++ src/com/jimweller/cpuscheduler/RoundRobinSchedulingAlgorithm.java
@@ -3,6 +3,7 @@
  * A scheduling algorithm that randomly picks the next job to go.
  *
  * @author: Erik Westrup & Andrew Maltun
+ * Winter 2013
  *
  */
 package com.jimweller.cpuscheduler;
@@ -13,23 +14,50 @@ public class RoundRobinSchedulingAlgorithm extends BaseSchedulingAlgorithm {
 
     /** the timeslice each process gets */
     private int quantum;
-
+    private PriorityQueue<Process> pQ; 
+    private Vector<Process> procs; 
+    private int jobsAdded; 
+    private int currentJob; 
+    
     RoundRobinSchedulingAlgorithm() {
+    	pQ = new PriorityQueue<Process>(8, new FCFSComparator());
+    	procs = new Vector<Process>();
+    	jobsAdded = 0;
+    	currentJob = 0;
     }
 
     /** Add the new job to the correct queue. */
     public void addJob(Process p) {
-
+    	
+    	/** Need to use a prio queue here because we don't know what order jobs will be 
+    	 * sent in. The prio queue sorts the jobs as they come in. We will process the prio 
+    	 * queue later into something we can iterate through.     	
+    	 */
+    	pQ.add(p);
+    	
+    	/** jobsAdded variable helps later in processing prio queue into iterable */ 
+    	jobsAdded++;
     }
 
     /** Returns true if the job was present and was removed. */
     public boolean removeJob(Process p) {
-    	return false; // TODO
+    	boolean result = false;
+    	if (!pQ.isEmpty()) {
+    		result = pQ.remove(p);
+    		procs.remove(p);
+    	}
+    	return result;
     }
 
     /** Transfer all the jobs in the queue of a SchedulingAlgorithm to another, such as
 	when switching to another algorithm in the GUI */
     public void transferJobsTo(SchedulingAlgorithm otherAlg) {
+    	System.out.println("Transfer in progress...");
+    	Iterator<Process> iter = pQ.iterator();
+    	while (iter.hasNext()) {
+			Process p = iter.next();
+    		otherAlg.addJob(p);
+    	}
     }
 
     /**
@@ -38,7 +66,7 @@ public class RoundRobinSchedulingAlgorithm extends BaseSchedulingAlgorithm {
      * @return Value of quantum.
      */
     public int getQuantum() {
-		return quantum;
+	return quantum;
     }
 
     /**
@@ -56,7 +84,23 @@ public class RoundRobinSchedulingAlgorithm extends BaseSchedulingAlgorithm {
      * available.
      */
     public Process getNextJob(long currentTime) {
-    	return null; // TODO
+    	
+    	/** If any jobs have been added, need to strip them off the prio queue 
+    	 * and put them at the end of the line for RR processing. */
+    	while (jobsAdded > 0) {
+    		procs.add(pQ.poll());
+    		jobsAdded--; 
+    	}
+    	
+    	/** If time quantum has run out, advance to the next job. 
+    	 * Loop back to 0 once the currentJob counter reaches the end of the procs vector.  
+    	 * */
+    	if (currentTime % quantum == 0) {
+    		currentJob = currentJob==procs.size() ? 0: currentJob + 1;
+    	}
+    	
+    	return (procs.get(currentJob)); 
+    	
     }
 
     public String getName() {
