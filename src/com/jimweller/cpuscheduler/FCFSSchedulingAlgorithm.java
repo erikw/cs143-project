4a0cda53421c3a57e7ca28c04c1c771bc78061c8
Erik Westrup <erik.westrup@gmail.com>
2013-02-20 16:49:15 -0800

    Deleted out-commented code for FCFS.

--- src/com/jimweller/cpuscheduler/FCFSSchedulingAlgorithm.java
+++ src/com/jimweller/cpuscheduler/FCFSSchedulingAlgorithm.java
@@ -11,45 +11,6 @@ package com.jimweller.cpuscheduler;
 import java.util.*;
 
 public class FCFSSchedulingAlgorithm extends BaseSchedulingAlgorithm {
-
-    // Naive implementation below, not correct?
-  
-    //private List<Process> que;
-
-    //FCFSSchedulingAlgorithm() {
-		//que = new LinkedList<Process>();
-    //}
-
-    //[>* Add the new job to the correct queue.<]
-    //public void addJob(Process p) {
-        ////System.out.println("Adding jobb");
-		//que.add(p);
-    //}
-
-    //[>* Returns true if the job was present and was removed. <]
-    //public boolean removeJob(Process p) {
-		//return que.remove(p);
-    //}
-
-    //[>* Transfer all the jobs in the queue of a SchedulingAlgorithm to another, such as
-	  //when switching to another algorithm in the GUI */
-    //public void transferJobsTo(SchedulingAlgorithm otherAlg) {
-        ////System.out.println("Transfer in progress.");
-        //Iterator<Process> iter = que.iterator();
-        //while (iter.hasNext()) {
-			//Process p = iter.next();
-            //otherAlg.addJob(p);
-			//iter.remove();
-        //}
-    //}
-
-
-    //[>* Returns the next process that should be run by the CPU, null if none available.<]
-    //public Process getNextJob(long currentTime) {
-        //return que.get(0);
-    //}
-
- 
     private PriorityQueue<Process> pque;
 
     FCFSSchedulingAlgorithm() {
